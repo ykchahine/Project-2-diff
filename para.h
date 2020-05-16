aa96165e9d61331329a46e7853384f8b71db67ab
ykchahine <42751366+ykchahine@users.noreply.github.com>
2020-05-16 12:53:10 -0700

    Update para.h

--- para.h
+++ para.h
@@ -31,6 +31,6 @@ char* para_info(para* p);
 int   para_equal(para* p, para* q);
 void para_print(para* p, void (*fp)(const char*));
 void para_printfile(char* base[], int count, void (*fp)(const char*));
-
+int para_almostequal(para* p, para* q, int* lines_equal, int nlines);
 
 #endif /* para_h */
